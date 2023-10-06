9dba36dc636c2276ac95ab55eb72c2ddd196ce1a
Bré Dale <121528456+mxbrelanae@users.noreply.github.com>
2023-10-06 10:55:37 -0400

    Update wordfinder.py

--- wordfinder.py
+++ wordfinder.py
@@ -12,7 +12,7 @@ class WordFinder:
         print(f"{len(self.words)} words read")
 
     def parse(self, word_file):
-        """Read through the list of words in "word_file". """
+        """Read through the list of words in "word_file". Also strips the whitespace and # character from infront or behind each word """
         return [w.strip() for w in word_file
                 if w.strip() and not w.startswith("#")]
        
